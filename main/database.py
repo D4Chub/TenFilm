b3d57874e43a28a4d7748bb48fe6bd0901267a6e
D4Chub <akbievalim0578@gmail.com>
2023-12-12 15:42:36 +0300

    aaa

--- main/database.py
+++ main/database.py
@@ -3,7 +3,12 @@ import sqlite3
 conn = sqlite3.connect('mydatabase.db')
 
 sql = 'CREATE TABLE movies (link TEXT, image_link TEXT, name VARCHAR, rate VARCHAR)'
+sql = 'SELECT * FROM movies'
 cursor = conn.cursor()
-
 cursor.execute(sql)
+res = cursor.fetchall()
+
+# for r in res:
+    # print("Name:", r[2])
+
 conn.close()
--- tenfilm/settings.py
+++ tenfilm/settings.py
@@ -78,9 +78,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+
+    'mydatabase': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'mydatabase.db',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
